372e966720eee2aaeb246b6137c28d0e303fba70
wwj <1744131094@qq.com>
2018-10-19 12:26:50 +0800

    解决合并冲突

--- app/src/main/java/com/dasu/customrecyclerview/MainActivity.java
+++ app/src/main/java/com/dasu/customrecyclerview/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
